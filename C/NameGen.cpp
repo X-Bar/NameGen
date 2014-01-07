7fe1af5498b2d25a0d190bfc134c81c6728af186
Cody <cody@mail.example.com>
2014-01-07 01:45:04 -0600

    X-Bar: added c file converting python code to c code

---
+++ C/NameGen.cpp
@@ -0,0 +1,19 @@
+/***********************************************************************************************************************
+FILENAME:   NameGen.cpp
+AUTHORS:    Cody L. Lundberg
+
+DESCRIPTION:
+Name generator that can read names from files to create different random cultural names
+* name parts
+* gender (male, female)
+* type (first last) 
+all held in 3 different files (male first, female first, last)
+user selects gender and culture and receives names
+
+Convertion from python code
+
+REVISION HISTORY:
+01.07.2014   CDM     Cody original file creation
+***********************************************************************************************************************/
+
+
