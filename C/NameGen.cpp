df9f610d97ef5fb7226c66ebd37d55db9ebfbdde
CodyL <codyl@uta.edu>
2019-09-03 17:56:13 -0500

    Updated make file for c++11/STL and move code into class

--- C/NameGen.cpp
+++ C/NameGen.cpp
@@ -19,6 +19,10 @@ REVISION HISTORY:
 #include <stdio.h>
 #include <unistd.h>
 #include <stdlib.h>
+
+#include <vector>
+#include <string>
+
 //~ #include "boost/filesystem/operations.hpp"
 //~ #include "boost/filesystem/path.hpp"
 //~ #include "boost/progress.hpp"
@@ -29,43 +33,94 @@ REVISION HISTORY:
 
 
 
-
-int main(void)
-{		
-	// http://www.dreamincode.net/forums/topic/60036-accessing-directories-in-cc-part-ii/
-	DIR *pdir = NULL;
-	struct dirent *pent = NULL;
-
-	if (chdir("../Names") != 0)
-		perror("chdir() to /Names failed");
+class GenFileNames
+{
+private: // private data
+	DIR *pdir;
 	
-	pdir = opendir ("."); 										// "." will refer to the current directory
-	if (pdir == NULL) 											// if pdir wasn't initialised correctly
-	{ 															// print an error message and exit the program
-		printf ("\nERROR! pdir could not be initialised correctly");
-		exit (1);	 											// exit the program, using 1 as the status (most common for a failed execution)
-	} // end if
+	
+	
+public: // public data
 
-	while (pent = readdir (pdir)) 								// while there is still something in the directory to list
+private: // private methods
+	
+	
+	const std::vector<std::string> SplitString(const std::string& s, const char& c)
 	{
-		if (pent == NULL) 										// if pent has not been initialised correctly
-		{ 														// print an error message, and exit the program
-			printf ("ERROR! pent could not be initialised correctly");
-			exit (3);
+		// http://www.cplusplus.com/articles/2wA0RXSz/
+		std::string buff{""};
+		std::vector<std::string> v;
+		
+		for(auto n:s)
+		{
+			if(n != c) buff+=n; else
+			if(n == c && buff != "") { v.push_back(buff); buff = ""; }
 		}
-																// otherwise, it was initialised correctly. let's print it on the console:
-		printf ("%s\n", pent->d_name);
+		if(buff != "") v.push_back(buff);
+		
+		return v;
 	}
 	
-	
+public: // public methods
+	GenFileNames(void) 									// constructor
+	{
+		// http://www.dreamincode.net/forums/topic/60036-accessing-directories-in-cc-part-ii/
+		pdir = NULL;
+		struct dirent *pent = NULL;
 
+		if (chdir("../Names") != 0)
+			perror("chdir() to /Names failed");
+		
+		pdir = opendir ("."); 										// "." will refer to the current directory
+		if (pdir == NULL) 											// if pdir wasn't initialised correctly
+		{ 															// print an error message and exit the program
+			printf ("\nERROR! pdir could not be initialised correctly");
+			exit (1);	 											// exit the program, using 1 as the status (most common for a failed execution)
+		} // end if
+
+		while ( (pent = readdir (pdir) ) ) 							// while there is still something in the directory to list
+		{
+			if (pent == NULL) 										// if pent has not been initialised correctly
+			{ 														// print an error message, and exit the program
+				printf ("ERROR! pent could not be initialised correctly");
+				exit (2);
+			}
+			printf ("Found File: %s\n", pent->d_name);				// otherwise, it was initialised correctly. let's print it on the console:
+			std::string FileName(pent->d_name);
+			std::vector<std::string> NameSplit = SplitString(FileName, '_');
+			if ( (NameSplit.size() < 3) )
+			{	
+				printf("File not NameFile. Not dilimited.\n");
+			}
+			else if (NameSplit[0] != "NameFile")
+			{
+				printf("File not NameFile. Incorrect prefix.\n");
+			}
+			else
+			{
+				printf("Culture %s\n", NameSplit[1].c_str());
+				// use std::set to track culture types. this will automaticly remove duplicates and sort
+			}
+		}
+	}
+		
+	~GenFileNames(void) 											// deconstuctor
+	{
+		if (pdir != NULL)
+		{
+			closedir (pdir);
+		}
+		
+	}
 	
+}; // end class GenFileNames
+
+
+int main(void)
+{
+	GenFileNames GFN;
+	//~ GFN
 	
-	
-	
-	
-	
-	closedir (pdir);
     return 0; // everything went OK
 	
 }
